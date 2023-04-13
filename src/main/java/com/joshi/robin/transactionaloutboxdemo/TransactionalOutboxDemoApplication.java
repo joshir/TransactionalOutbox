11a790584982be8181f163670bf7f8f45681d06e
joshir <hareepjoshi@gmail.com>
2023-04-12 22:30:50 -0500

    set up posgres and flyway migration

--- src/main/java/com/joshi/robin/transactionaloutboxdemo/TransactionalOutboxDemoApplication.java
+++ src/main/java/com/joshi/robin/transactionaloutboxdemo/TransactionalOutboxDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransactionalOutboxDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionalOutboxDemoApplication.class, args);
 	}
-
 }
